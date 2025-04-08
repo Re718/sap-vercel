9d543578788b83833bfc7980455c28144811f299
Re718 <hyma.desiraju@compass-usa.com>
2025-04-08 15:25:23 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="p-4 text-center text-xl font-semibold text-green-700">
+      ✅ SAP Material Impact Visualizer is working!
+    </div>
+  );
+}
